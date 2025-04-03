7fb4534bba81a049a300107a7aa7f8e1e49c849d
Jessica Laceste <jessica.laceste@lorma.edu>
2025-04-04 00:41:27 +0800

    Create CoffeeCrudApplication.java

---
+++ CoffeeCrudApplication.java
@@ -0,0 +1,16 @@
+package com.kakang.CRUD_Coffee;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+
+@SpringBootApplication
+public class CoffeeCrudApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeCrudApplication.class, args);
+    }
